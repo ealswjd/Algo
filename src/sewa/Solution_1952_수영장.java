cee3288d7bab70762c0308160165635a985baf34
ealswjd <mjjr0424@naver.com>
2022-11-16 22:24:01 +0900

    1952

--- src/sewa/Solution_1952_수영장.java
+++ src/sewa/Solution_1952_수영장.java
@@ -1,12 +1,14 @@
 package sewa;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution_1952_수영장 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
 	}//main
 
