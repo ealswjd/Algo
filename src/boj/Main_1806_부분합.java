790c1ad6948427ce33f71acfdc6dc9ffb7d32dc5
ealswjd <mjjr0424@naver.com>
2023-03-19 23:15:16 +0900

    23.03.19 boj_1806

---
+++ src/boj/Main_1806_부분합.java
@@ -0,0 +1,51 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1806
+public class Main_1806_부분합 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		br.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(getLength(arr, n, s) + "\n");
+		bw.flush();
+		bw.close();
+	}//main
+    
+    static int getLength(int[] arr, int n, int s) {
+        int min = Integer.MAX_VALUE;
+		int length = 0;
+		int start = 0;
+		int end = 0;
+		int sum = 0;
+        
+		while(true) {			
+			if(sum >= s) {
+				sum -= arr[start++];
+				min = Math.min(min, (end-start) + 1);
+				length = min;
+			}
+			else if(end == n) break;
+			else sum += arr[end++];
+		}//while	
+        
+		return length;
+	}//getLength
+	
+}// Main class
