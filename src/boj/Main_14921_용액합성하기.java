9d5d5fe0a60f8975c22a305548204368a7b60971
ealswjd <mjjr0424@naver.com>
2023-04-24 20:32:29 +0900

    23.04.24 boj_14921_용액합성하기

---
+++ src/boj/Main_14921_용액합성하기.java
@@ -0,0 +1,50 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_14921_용액합성하기 {
+	static int N; // 용액 수
+	static int[] arr; // 용액
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>( (o1, o2) -> {return Math.abs(o1) - Math.abs(o2);} );
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 정렬
+		for(int i=0; i<N; i++) {
+			pq.offer(Integer.parseInt(st.nextToken()));
+		}//for
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = pq.poll();
+		}//for
+		
+		System.out.print(getMin());
+	}//main
+
+	private static int getMin() {
+		int min = Integer.MAX_VALUE;
+		int ans = min;
+		
+		int start = 0;
+		int end = start+1;
+		
+		while(end < N) {
+			if(min > Math.abs(arr[start]+arr[end])) {
+				min = Math.abs(arr[start]+arr[end]);
+				ans = arr[start]+arr[end];
+			}
+			start++;
+			end++;
+		}//while
+		
+		return ans;
+	}//getMin
+
+}//class
