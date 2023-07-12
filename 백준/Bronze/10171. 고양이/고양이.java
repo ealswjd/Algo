261d5cb30ec1fd42df49b14cab4725eae754e790
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-07-12 19:28:42 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11180 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder ans = new StringBuilder();
+		ans.append("\\    /\\").append("\n");
+		ans.append(" )  ( ')").append("\n");
+		ans.append("(  /  )").append("\n");
+		ans.append(" \\(__)|");
+		
+		System.out.println(ans);
+	}//main
+
+}//class
