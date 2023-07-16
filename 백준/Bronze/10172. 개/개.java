7a844fffddf5f333f817cfa2866a4b7a4867cbbd
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-07-16 22:27:02 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder ans = new StringBuilder();
+		ans.append("|\\_/|").append("\n");
+		ans.append("|q p|   /}").append("\n");
+		ans.append("( 0 )\"\"\"\\").append("\n");
+		ans.append("|\"^\"`    |").append("\n");
+		ans.append("||_/=\\\\__|");
+		
+		System.out.print(ans);
+	}//main
+
+}//class
