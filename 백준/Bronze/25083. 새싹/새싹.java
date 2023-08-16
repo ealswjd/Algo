c21dc75ec81218de6d30bee4ad574e9fcf6e77be
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-08-16 21:52:54 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder ans = new StringBuilder();
+		ans.append("         ,r'\"7").append("\n")
+			.append("r`-_   ,'  ,/").append("\n")
+			.append(" \\. \". L_r'").append("\n")
+			.append("   `~\\/").append("\n")
+			.append("      |").append("\n")
+			.append("      |");
+			
+		System.out.print(ans);
+	}//main
+
+}//class
