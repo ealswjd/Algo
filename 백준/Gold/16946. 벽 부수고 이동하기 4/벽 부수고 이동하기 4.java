f7f0cc84c9e1ae26913ed08715226cf7e849400a
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-08-22 16:16:16 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 4268 ms, Memory: 322884 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -8,11 +7,9 @@ import java.util.StringTokenizer;
 // 벽 부수고 이동하기 4
 public class Main {
 	static int N, M, areaNum;
-	static boolean[][] visited;
-	static boolean[][] isWall;
-	static ArrayList<Integer> cnts;
-	static int[][] area;
-	static Queue<int[]> q;
+	static int[][] visited;
+	static char[][] map;
+	static int[] cnts;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 
@@ -21,56 +18,50 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine()); 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N][M];
-		isWall = new boolean[N][M];
-		area = new int[N][M];
-		cnts = new ArrayList<>();
-		
-		q = new LinkedList<>();
+		visited = new int[N][M];
+		map = new char[N][M];
+		cnts = new int[100000];
+
 		for(int i=0; i<N; i++) {
-			char[] row = br.readLine().toCharArray();
-			for(int j=0; j<M; j++) {
-				if(row[j] == '0') q.offer(new int[] {i, j});										
-				else isWall[i][j] = true;				
-			}//for j
+			map[i] = br.readLine().toCharArray();			
 		}//for i
 		br.close();
 
-		breakWall(); 
+		areaNum = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(map[i][j] == '1' || visited[i][j] > 0) continue;
+				visited[i][j] = ++areaNum;
+				getArea(i, j);
+			}//for j
+		}//for i
+		
 		printAnswer();		
 	}//main
 
-	private static void breakWall() {
+
+	private static void getArea(int r, int c) {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {r, c});
+		int cnt = 1;
 		int[] cur;
-		int r, c;
-		areaNum = 0;
-		int cnt = 0;
 		while(!q.isEmpty()) {
 			cur = q.poll();
 			r = cur[0];
 			c = cur[1];
-			if(visited[r][c]) continue;
-			areaNum++;
-			cnt = getCnt(r, c, 0);
-			if(cnt == 0) cnt = 1;
-			cnts.add(cnt);
+			for(int i=0; i<4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+				if(rangeCheck(nr, nc) || visited[nr][nc] > 0 || map[nr][nc] == '1') continue;
+				cnt++;
+				visited[nr][nc] = areaNum;
+				q.offer(new int[] {nr, nc});
+			}//for
 		}//while
-
-	}//breakWall
-
-	private static int getCnt(int r, int c, int cnt) {
 		
-		for(int i=0; i<4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if(rangeCheck(nr, nc) || visited[nr][nc] || isWall[nr][nc]) continue;
-			visited[nr][nc] = true;
-			cnt = getCnt(nr, nc, cnt+1);
-		}//for
-		
-		area[r][c] = areaNum;
-		return cnt;
-	}//getCnt
+		cnts[areaNum] = cnt;
+	}//getArea
+
 
 	private static boolean rangeCheck(int r, int c) {
 		return r < 0 || r >= N || c < 0 || c >= M;
@@ -80,7 +71,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
-				if(!isWall[i][j]) sb.append(0);
+				if(map[i][j] == '0') sb.append(0);
 				else sb.append(getSum(i, j));
 			}//for j
 			sb.append("\n");
@@ -96,9 +87,9 @@ public class Main {
 		for(int i=0; i<4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
-			if(rangeCheck(nr, nc) || isWall[nr][nc] || checked[area[nr][nc]]) continue;
-			checked[area[nr][nc]] = true;
-			sum += cnts.get(area[nr][nc]-1);
+			if(rangeCheck(nr, nc) || map[nr][nc] == '1' || checked[visited[nr][nc]]) continue;
+			checked[visited[nr][nc]] = true;
+			sum += cnts[visited[nr][nc]];
 		}//for
 		
 		return sum % 10;
