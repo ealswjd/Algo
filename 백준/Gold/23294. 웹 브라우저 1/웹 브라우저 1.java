a7399ef55c8d2bf79a282a3bdccf0a1e1f74a7f9
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-09-20 20:49:12 +0900

    [Gold IV] Title: 웹 브라우저 1, Time: 140 ms, Memory: 21236 KB -BaekjoonHub

--- 백준/Gold/23294. 웹 브라우저 1/웹 브라우저 1.java
+++ 백준/Gold/23294. 웹 브라우저 1/웹 브라우저 1.java
@@ -1,27 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/23294
 public class Main {
 	static int N, C, curPage, curSize; 
-	static int[] cSize; // 페이지 캐시 크기
+	static int[] cSize; // 웹페이지 캐시 크기
 	static Deque<Integer> back; // 뒤로 가기 공간
 	static Deque<Integer> front; // 앞으로 가기 공간
-	static ArrayList<Integer> tmp; // 임시 공간
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 접속할 수 있는 웹페이지의 종류의 수
-		int Q = Integer.parseInt(st.nextToken()); // 사용자가 수행하는 작업의 개수
+		N = Integer.parseInt(st.nextToken()); // 웹페이지의 종류의 수
+		int Q = Integer.parseInt(st.nextToken()); // 작업의 개수
 		C = Integer.parseInt(st.nextToken()); // 최대 캐시 용량
 		
-		cSize = new int[N+1]; // 페이지 캐시 크기
+		cSize = new int[N+1]; // 웹페이지 캐시 크기
 		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++) {
 			cSize[i] = Integer.parseInt(st.nextToken());
@@ -39,14 +36,14 @@ public class Main {
 
 	private static void print() {
 		StringBuilder ans= new StringBuilder();				
-		ans.append(curPage).append("\n"); // 현재 페이지 번호 출력		
+		ans.append(curPage).append("\n"); // 현재 페이지 번호		
 		outputInOrder(back, ans); // 뒤로 가기 공간		
 		outputInOrder(front, ans); // 앞으로 가기 공간 
 		System.out.print(ans);
 	}//print
 
 	private static void outputInOrder(Deque<Integer> cache, StringBuilder ans) {
-		// 가장 최근에 방문한 순서대로 페이지 번호 출력
+		// 가장 최근에 방문한 순서대로 페이지의 번호를 출력
 		if(cache.isEmpty()) ans.append(-1);
 		else {
 			while(!cache.isEmpty()) {
@@ -68,28 +65,22 @@ public class Main {
 			break;
 		case 'C' : // 압축
 			if(back.isEmpty()) break;
-			
-			Iterator<Integer> it = back.iterator();			
-			int prev = it.next();
-			tmp = new ArrayList<>();
-			tmp.add(prev);
+	
+			int prev = back.poll();
+			int size = back.size();
+			back.add(prev);
 			int cur;
 			
-			while(it.hasNext()) {
-				cur = it.next();
+			while(size-->0) {
+				cur = back.poll();
 				/* 삭제된 페이지가 차지하고 있던 용량만큼 현재 사용 캐시에서 줄어든다. */
 				if(cur == prev) {
 					curSize -= cSize[cur];
 					continue;
 				}//if
-				tmp.add(cur);
+				back.add(cur);
 				prev = cur;
 			}//while
-			
-			back.clear();
-			for(int n : tmp) {
-				back.offer(n);
-			}//for
 			
 			break;
 		default :  // 접속
@@ -98,11 +89,12 @@ public class Main {
 			while(!front.isEmpty()) {
 				curSize -= cSize[front.poll()];
 			}//while
-
+			/* 현재 페이지를 뒤로 가기 공간에 추가하고, 다음에 접속할 페이지가 현재 페이지로 갱신*/
 			if(curPage != 0) back.add(curPage);
 			curPage = page;
 			curSize += cSize[curPage];
-
+			/* 뒤로 가기 공간에서 방문한 지 가장 오래된 페이지 하나를 삭제하며, 
+			 * 그 페이지가 차지하고 있던 크기가 현재 사용 캐시 용량에서 줄어든다. */
 			if(curSize > C) {
 				while(!back.isEmpty() && curSize > C) {
 					curSize -= cSize[back.pollFirst()];
@@ -114,11 +106,8 @@ public class Main {
 	}//work
 
 	private static void move(Deque<Integer> cache1, Deque<Integer> cache2) {
-		/* cache1 공간에 저장된 페이지가 0개일 때 이 작업은 무시 */
 		if(cache1.isEmpty()) return;
-		/* 현재 보고 있던 웹페이지를 cache2 공간에 저장 */
-		cache2.add(curPage);
-		/* cache1 공간에서 방문한지 가장 최근의 페이지에 접속 */
+		cache2.add(curPage);		
 		curPage = cache1.pollLast();		
 	}//move
 
