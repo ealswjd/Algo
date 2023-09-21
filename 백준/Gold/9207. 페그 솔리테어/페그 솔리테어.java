5c30c5124e7b76cc0169298e6dd0a6498b1a7a17
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-09-21 16:42:11 +0900

    [Gold IV] Title: 페그 솔리테어, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.java
+++ 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 // https://www.acmicpc.net/problem/9207
 public class Main {
 	static final int R=5, C=9;
 	static final char PIN='o', EMPTY='.';
-	static char[][] map; // 게임판
-	static int minPinCnt, minMoveCnt, total; // 핀의 최소 개수, 최소 이동 횟수, 총 개수
+	static int[][] map; // 게임판
+	static int minPinCnt, minMoveCnt, total; // 핀의 최소 개수, 최소 이동 횟수
+	static ArrayList<int[]> pinList; // 핀
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 	
@@ -17,22 +19,26 @@ public class Main {
 		StringBuilder ans = new StringBuilder();
 		
 		while(T-->0) {
-			map = new char[R][C]; // 게임판
+			map = new int[R][C]; // 게임판
+			pinList = new ArrayList<>();
 			total = 0;
 			for(int i=0; i<R; i++) {
-				map[i] = br.readLine().toCharArray();
+				char[] tmp = br.readLine().toCharArray();
 				for(int j=0; j<C; j++) {
-					if(map[i][j] == PIN) total++;
+					map[i][j] = tmp[j];
+					if(map[i][j] == PIN) {
+						pinList.add(new int[] {i, j, 1});
+						map[i][j] = total++;
+					}
 				}//for j
 			}//for i
 			
 			minPinCnt = total; // 핀의 최소 개수
 			minMoveCnt = 0; // 최소 이동 횟수
-			for(int i=0; i<R; i++) {
-				for(int j=0; j<C; j++) {
-					if(map[i][j] == PIN) move(i, j, total, 0);					
-				}//for j
-			}//for i
+			
+			for(int i=0; i<total; i++) {
+				move(i, total, 0);
+			}//for
 			
 			ans.append(minPinCnt).append(" ");
 			ans.append(minMoveCnt).append("\n");
@@ -43,29 +49,46 @@ public class Main {
 		System.out.print(ans);
 	}//main
 
-	private static void move(int r, int c, int pinCnt, int moveCnt) {
+	private static void move(int start, int pinCnt, int moveCnt) {
 		if(pinCnt <= minPinCnt) {
 			minPinCnt = pinCnt;
 			minMoveCnt = moveCnt;			
-		}//if
+		}//if	
+		
+		int[] cur = pinList.get(start);
+		int r = cur[0];
+		int c = cur[1];
+		int status = cur[2];
+		if(status == 0) return;
+		int num = map[r][c];
 		
 		for(int i=0; i<4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
-			if(rangeCheck(nr, nc) || map[nr][nc] != PIN || pinCnt - 1 < 1) continue;
-			if(rangeCheck(nr+dr[i], nc+dc[i]) || map[nr+dr[i]][nc+dc[i]] != EMPTY) continue;
-			
+			if(rangeCheck(nr, nc) || !(0<=map[nr][nc] && map[nr][nc]<8) || pinCnt - 1 < 1) continue;
+			int nnr = nr+dr[i];
+			int nnc = nc+dc[i];
+			if(rangeCheck(nnr, nnc) || map[nnr][nnc] != EMPTY) continue;
+			int next = map[nr][nc];
+
 			map[r][c] = map[nr][nc] = EMPTY;
-			map[nr+dr[i]][nc+dc[i]] = PIN;
+			map[nnr][nnc] = num;
+			pinList.get(start)[0] = nnr;
+			pinList.get(start)[1] = nnc;		
+			pinList.get(next)[2] = 0;
+			
+			for(int n=0; n<total; n++) {
+				if(pinList.get(n)[2] == 0) continue;
+				move(n, pinCnt-1, moveCnt+1);
+			}//for
+			
+			map[r][c] = num;
+			map[nr][nc] = next;
+			map[nnr][nnc] = EMPTY;
+			pinList.get(start)[0] = r;
+			pinList.get(start)[1] = c;		
+			pinList.get(next)[2] = 1;
 
-			for(int n=0; n<R; n++) {
-				for(int m=0; m<C; m++) {
-					if(map[n][m] == PIN) move(n, m, pinCnt-1, moveCnt+1);					
-				}//for j
-			}//for i
-
-			map[r][c] = map[nr][nc] = PIN;
-			map[nr+dr[i]][nc+dc[i]] = EMPTY;
 		}//for
 
 	}//move
