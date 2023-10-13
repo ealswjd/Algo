489d1e063cfeb14bdac54ba05cd1446ef9cf4acc
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-10-13 19:05:58 +0900

    [Silver IV] Title: 상근이의 여행, Time: 188 ms, Memory: 22808 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -8,19 +8,15 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine()); 
-		
-		StringTokenizer st;
+
 		StringBuilder ans = new StringBuilder();
+		StringTokenizer st;
 		while(T-->0) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // 국가 수
-			int M = Integer.parseInt(st.nextToken()); // 비행기 종류
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 			
-			while(M-->0) {
-				st = new StringTokenizer(br.readLine());
-				Integer.parseInt(st.nextToken());
-				Integer.parseInt(st.nextToken());
-			}//while		
+			while(M-->0) br.readLine();
 			
 			ans.append(N-1).append("\n");
 		}//while
