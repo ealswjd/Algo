89839e6a5d151cefc7de166f1b075c7f86a84216
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-10-18 15:28:08 +0900

    [Gold III] Title: 문자판, Time: 368 ms, Memory: 17164 KB -BaekjoonHub

--- 백준/Gold/2186. 문자판/문자판.java
+++ 백준/Gold/2186. 문자판/문자판.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -12,7 +11,6 @@ public class Main {
 	static int[][][] dp;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
-	static ArrayList<ArrayList<int[]>> positions;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,24 +18,30 @@ public class Main {
 		N = Integer.parseInt(st.nextToken()); // 행
 		M = Integer.parseInt(st.nextToken()); // 열
 		K = Integer.parseInt(st.nextToken()); // 이동 가능한 칸 개수
-		
-		init();
-		
+
+		map = new char[N][M]; // 문자판
 		for(int i=0; i<N; i++) {
 			map[i] = br.readLine().toCharArray();
-			for(int j=0; j<M; j++) {
-				positions.get(map[i][j] - 'A').add(new int[] {i, j});				
-			}//for j
 		}//for i
 		
 		word = br.readLine().toCharArray(); // 만들어야 되는 단어
 		br.close();
 
 		len = word.length;
+		
+		dp = new int[N][M][len];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				Arrays.fill(dp[i][j], -1);				
+			}//for j
+		}//for i
+		
 		int result = 0;
-		for(int[] position : positions.get(word[0]-'A')) {
-			result += getCnt(position[0], position[1], 0);
-		}//for
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				if(map[r][c] == word[0]) result += getCnt(r, c, 0);				
+			}//for c
+		}//for r
 		
 		System.out.print(result);
 	}//main
@@ -62,20 +66,5 @@ public class Main {
 	private static boolean rangeCheck(int r, int c) {
 		return r < 0 || r >= N || c < 0 || c >= M;
 	}//rangeCheck
-
-	private static void init() {		
-		positions = new ArrayList<>();
-		for(int i=0; i<26; i++) {
-			positions.add(new ArrayList<>());
-		}//for
-		
-		map = new char[N][M]; // 문자판
-		dp = new int[N][M][81];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				Arrays.fill(dp[i][j], -1);				
-			}//for j
-		}//for i
-	}//init
 
 }//class
