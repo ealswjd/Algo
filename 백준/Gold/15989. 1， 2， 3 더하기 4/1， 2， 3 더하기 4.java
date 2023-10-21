c3d136b0434aa665c0a83dd915b9cdcb121249bb
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-10-21 09:35:53 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 84 ms, Memory: 11932 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -2,33 +2,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int[] dp;
+	static int[][] dp;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
+		getCnt();
+		
 		StringBuilder ans = new StringBuilder();
 		while(T-->0) {
 			int N = Integer.parseInt(br.readLine());
-			ans.append(getCnt(N)).append("\n");
+			ans.append(dp[3][N]).append("\n");
 		}//while
 		br.close();
 
 		System.out.print(ans);
 	}//main
 
-	private static int getCnt(int N) {
-		dp = new int[N+1];
-		dp[0] = 1;
+	private static void getCnt() {
+		dp = new int[4][10001];
+		dp[0][0] = 1;
 		
 		for(int num=1; num<=3; num++) {
-			for(int i=num; i<=N; i++) {
-				dp[i] += dp[i-num];
+			for(int i=1; i<=10000; i++) {
+				if(num==i) dp[num][i] = Math.max(dp[num-1][i], dp[num][i-1]) + 1;
+				else if(num>i) dp[num][i] = dp[num-1][i];
+				else dp[num][i] += dp[num-1][i] + dp[num][i-num];				
 			}//for i
 		}//for num
-		
-		return dp[N];
+
 	}//getCnt
 
 }//class
