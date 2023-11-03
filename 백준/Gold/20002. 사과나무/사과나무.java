e3ee7e16bb423396756233bdf11f06b9006986d6
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-11-03 10:31:39 +0900

    [Gold V] Title: 사과나무, Time: 308 ms, Memory: 21396 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.java
+++ 백준/Gold/20002. 사과나무/사과나무.java
@@ -5,46 +5,50 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/20002
 public class Main {
 	static int N, max;
-	static int[][] row;
+	static int[][] cost;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		row = new int[N+1][N+1];
+		int[][] map = new int[N][N];
 		max = -1000;
-		
+
 		StringTokenizer st;
-		int n;
-		for(int r=1; r<=N; r++) {
+		for(int r=0; r<N; r++) {
 			st = new StringTokenizer(br.readLine());			
-			for(int c=1; c<=N; c++) {
-				n = Integer.parseInt(st.nextToken());
-				max = Math.max(max, n);
-				row[r][c] = row[r][c-1] + n;
+			for(int c=0; c<N; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, map[r][c]);
 			}//for c
 		}//for r
 		br.close();
 		
-		for(int k=3; k<=N; k++) {
-			for(int i=k; i<=N; i++) {
-				for(int j=k; j<=N; j++) {
-					max = Math.max(max, getSum(k, i, j));
-				}//for j
-			}//for i
-			
-		}//for k
+		cost = new int[N+1][N+1];
+		for(int r=1; r<=N; r++) {
+			for(int c=1; c<=N; c++) {
+				cost[r][c] = map[r-1][c-1] + cost[r-1][c] + cost[r][c-1] - cost[r-1][c-1];
+			}//for c
+		}//for r
+		
+		for(int k=2; k<=N; k++) {
+			max = Math.max(max, getSum(k));
+		}//for
 		
 		System.out.print(max);
-
 	}//main
 
-	private static int getSum(int k, int r, int c) {
-		int start = r-k+1;
-		int sum = 0;
-		for(int i=start; i<=r; i++) {
-			sum += row[i][c] - row[i][c-k];
-		}//for
-		return sum;
+	private static int getSum(int k) {
+		int result = -1000;
+		int sum;
+		
+		for(int r=k; r<=N; r++) {
+			for(int c=k; c<=N; c++) {
+				sum = cost[r][c] - cost[r-k][c] - cost[r][c-k] + cost[r-k][c-k];
+				result = Math.max(result, sum);
+			}//for c
+		}//for r
+		
+		return result;
 	}//getSum
 
 }//class
