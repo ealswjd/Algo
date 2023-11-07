649f6f6da94b630728105a6b22bf3dab86f668ae
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-11-07 19:33:34 +0900

    [Gold V] Title: LCS, Time: 100 ms, Memory: 15752 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 // https://www.acmicpc.net/problem/9251
 public class Main {
 	static char[] A, B;
 	static int[][] dp;
-	static int max;
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,22 +14,18 @@ public class Main {
 		
 		int lenA = A.length;
 		int lenB = B.length;
-		dp = new int[lenA][lenB];
-		for(int i=0; i<lenA; i++) {
-			Arrays.fill(dp[i], -1);
-		}//for
 
-		System.out.println(lcs(lenA-1, lenB-1));
+		dp = new int[lenA+1][lenB+1];
+		System.out.print(lcs(lenA, lenB));
 	}//main
 
 	private static int lcs(int a, int b) {
-		if(a<0||b<0) return 0;
-		
-		if(dp[a][b] != -1) return dp[a][b];
-		
-		dp[a][b]=0;
-		if(A[a] == B[b]) dp[a][b] = lcs(a-1, b-1)+1;
-		else dp[a][b] = Math.max(lcs(a-1,b), lcs(a,b-1));
+		for(int r=1; r<=a; r++) {
+			for(int c=1; c<=b; c++) {
+				if(A[r-1] == B[c-1]) dp[r][c] = dp[r-1][c-1] + 1;
+				else dp[r][c] = Math.max(dp[r-1][c], dp[r][c-1]);
+			}//for c
+		}//for r
 		
 		return dp[a][b];
 	}//lcs
