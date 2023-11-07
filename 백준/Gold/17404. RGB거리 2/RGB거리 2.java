f789dc35047159546b216d484607cd74d80576c3
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-11-07 13:25:28 +0900

    [Gold IV] Title: RGB거리 2, Time: 88 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -28,23 +28,28 @@ public class Main {
 
 	private static int getMin() {
 		int min = INF;
+		
+		// 첫 번째 집 R, G, B 순서대로 칠함.
 		for(int k=0; k<3; k++) {
 			for(int i=0; i<3; i++) {
 				if(i==k) dp[0][i] = rgb[0][i];
 				else dp[0][i] = INF;
 			}//for i
 			
+			// 두 번째 집부터 R, G, B로 칠했을 때 누적 비용 최솟값 갱신
 			for(int i=1; i<N; i++) {
-				dp[i][R] = Math.min(dp[i-1][G], dp[i-1][B]) + rgb[i][R];
-				dp[i][G] = Math.min(dp[i-1][R], dp[i-1][B]) + rgb[i][G];
-				dp[i][B] = Math.min(dp[i-1][R], dp[i-1][G]) + rgb[i][B];
+				dp[i][R] = rgb[i][R] + Math.min(dp[i-1][G], dp[i-1][B]);
+				dp[i][G] = rgb[i][G] + Math.min(dp[i-1][R], dp[i-1][B]);
+				dp[i][B] = rgb[i][B] + Math.min(dp[i-1][R], dp[i-1][G]);
 			}//for i
 			
 			for(int i=0; i<3; i++) {
-				if(i == k) continue;
-				min = Math.min(min, dp[N-1][i]);
+				// N번 집의 색은 1번 집의 색과 같지 않아야 한다.
+				if(i == k) continue; // 1번 집의 색과 같을 경우 넘어감
+				min = Math.min(min, dp[N-1][i]); // 최솟값, 모든 집을 다 칠했을 때의 비용 비교
 			}//for i
 		}//for k
+		
 		return min;
 	}//getMin
 
