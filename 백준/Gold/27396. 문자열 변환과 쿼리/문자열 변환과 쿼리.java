4b1fc2f0f9e044fe371c1fd7c607fae177789327
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-12-11 22:16:46 +0900

    [Gold V] Title: 문자열 변환과 쿼리, Time: 1372 ms, Memory: 190320 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27396. 문자열 변환과 쿼리/문자열 변환과 쿼리.java
+++ 백준/Gold/27396. 문자열 변환과 쿼리/문자열 변환과 쿼리.java
@@ -1,30 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/27396
 public class Main {
 	static char[] str; // 문자열
-	static Map<Character, Queue<Integer>> map;
+	static char[] cArr;
 
 	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		map = new HashMap<>(); // 문자 인덱스 저장
-		initMap('A', 'Z');
-		initMap('a', 'z');
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		str = st.nextToken().toCharArray();
 		int N = Integer.parseInt(st.nextToken());
 		
-		for(int i=0, len=str.length; i<len; i++) {
-			map.get(str[i]).add(i);
-		}//for
+		cArr = new char[130];
+		for(char c='A'; c<='z'; c++) cArr[c] = c;
 		
 		StringBuilder ans = new StringBuilder();
 		int n;
@@ -36,10 +26,15 @@ public class Main {
 			case 1: // 문자 변경
 				a = st.nextToken().charAt(0);
 				b = st.nextToken().charAt(0);
-				change(a, b);
+				for(int i='A'; i<='z'; i++) {
+					if(cArr[i] == a) cArr[i] = b;
+				}//for
 				break;
 			case 2: // 문자 출력
-				ans.append(str).append("\n");
+				for(char c : str) {
+					ans.append(cArr[c]);
+				}//for
+				ans.append("\n");
 				break;
 			}//switch
 		}//while
@@ -48,23 +43,5 @@ public class Main {
 		System.out.print(ans);
 	}//main
 
-	private static void change(char a, char b) {
-		if(a == b) return;
-		Queue<Integer> q = map.get(a);
-		
-		int idx;
-		while(!q.isEmpty()) {
-			idx = q.poll();
-			str[idx] = b;
-			map.get(b).add(idx);
-		}//while
-		
-	}//change
-
-	private static void initMap(char start, char end) {
-		for(char c=start; c<=end; c++) {
-			map.put(c, new LinkedList<>());
-		}//for		
-	}//initMap
 
 }//class
