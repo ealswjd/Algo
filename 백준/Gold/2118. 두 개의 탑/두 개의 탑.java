dc65440d2148e7abda2c6c28c887f9b06c4f0d69
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-12-17 18:32:27 +0900

    [Gold V] Title: 두 개의 탑, Time: 160 ms, Memory: 17544 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -7,30 +7,27 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[] arr = new int[N+1];
 		int sum = 0;
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 			sum += arr[i];
 		}//for
 		
-		int max = 0;
-		int a = 0, b = 1;
-		int sumA = arr[0];
-		int sumB = sum - arr[0];
-		while(a < N) {
-			max = Math.max(max, Math.min(sumA, sumB));
-			if(sumA == sumB) break;
-			else if(sumA > sumB) {
-				sumA -= arr[a];
-				sumB += arr[a];
+		int min, max = 0;
+		int a = 0, b = 0;
+		int cur = arr[a];
+		
+		while(a <= b && b < N) {
+			min = Math.min(cur, sum-cur);
+			max = Math.max(max, min);
+			if(cur == min) {
+				b++;
+				cur += arr[b];
+			}else {
+				cur -= arr[a];
 				a++;
-				continue;
 			}
-			sumA += arr[b];
-			sumB -= arr[b];
-			b++;
-			b %= N;
 		}//while
 
 		System.out.print(max);
