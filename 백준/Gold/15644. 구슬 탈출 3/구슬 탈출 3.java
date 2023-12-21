39c7677a462d2f33640210bf2bd5f35f966d01bf
minjeong <79951187+ealswjd@users.noreply.github.com>
2023-12-21 21:44:46 +0900

    [Gold I] Title: 구슬 탈출 3, Time: 80 ms, Memory: 11748 KB -BaekjoonHub

--- 백준/Gold/15644. 구슬 탈출 3/구슬 탈출 3.java
+++ 백준/Gold/15644. 구슬 탈출 3/구슬 탈출 3.java
@@ -11,7 +11,7 @@ public class Main {
 	static final char EMPTY='.';
 	static final int[] dr = {-1, 1, 0, 0};
 	static final int[] dc = {0, 0, -1, 1};
-	static int N, M;
+	static int N, M, or, oc;
 	static char[][] map;
 
 	public static void main(String[] args) throws Exception{
@@ -37,6 +37,9 @@ public class Main {
 					b[0] = i;
 					b[1] = j;
 					map[i][j] = EMPTY;
+				}else if(map[i][j] == 'O') {
+					or = i;
+					oc = j;
 				}
 			}//for j
 		}//for i
@@ -52,7 +55,7 @@ public class Main {
 			for(int i=1; i<=cnt; i++) {
 				switch (result.order[i]) {
 				case U: c = 'U';					
-					break;
+				break;
 				case D: c = 'D';					
 				break;
 				case L: c = 'L';					
@@ -70,24 +73,20 @@ public class Main {
 		boolean[][][][] visited = new boolean[N][M][N][M];
 		visited[r[0]][r[1]][b[0]][b[1]] = true;
 		Queue<Position> q = new LinkedList<>();
-		for(int dir=0; dir<4; dir++) {
-			q.offer(new Position(r[0], r[1], b[0], b[1], 0, dir, new int[11]));
-		}//for
+		q.offer(new Position(r[0], r[1], b[0], b[1], 0, new int[11]));
 		
 		Position cur;
 		while(!q.isEmpty()) {
 			cur = q.poll();
 		
-			if(cur.redR == -1 && cur.redC== -1) return cur;
-			if(cur.cnt == 10 || cur.blueR == -1) continue;
+			if(cur.redR == or && cur.redC== oc) return cur;
+			if(cur.cnt == 10 || (cur.blueR == or && cur.blueC == oc)) continue;
 			
 			for(int i=0; i<4; i++) {
 				Position next = getNext(cur, i);
 				if(next == null) continue;
-				if(next.redR!=-1 && next.redC!=-1 && next.blueR!=-1 && next.blueC!=-1) {
-					if(visited[next.redR][next.redC][next.blueR][next.blueC]) continue;
-					visited[next.redR][next.redC][next.blueR][next.blueC] = true;
-				}
+				if(visited[next.redR][next.redC][next.blueR][next.blueC]) continue;
+				visited[next.redR][next.redC][next.blueR][next.blueC] = true;
 				next.order[cur.cnt+1] = i;
 				q.offer(next);
 			}//for
@@ -101,91 +100,64 @@ public class Main {
 		int rc = cur.redC;
 		int br = cur.blueR;
 		int bc = cur.blueC;
-		
-		boolean redFirst = getFirst(cur, dir);		
-		int goal = 0;
-		if(redFirst) { // 빨간 구슬 먼저 움직임
 
-			while(true) {
-				rr += dr[dir];
-				rc += dc[dir];
-				if(map[rr][rc] != EMPTY) {
-					if(map[rr][rc] == 'O') {
-						goal++;
-						rr = -1;
-						rc = -1;
-						break;
-					}//if
-					rr -= dr[dir];
-					rc -= dc[dir];
-					break;
-				}
-			}//while
-			
-			if(rr != -1) map[rr][rc] = 'R';
-			
-			while(true) {
-				br += dr[dir];
-				bc += dc[dir];
-				if(map[br][bc] != EMPTY) {
-					if(map[br][bc] == 'O') {
-						goal++;
-						br = -1;
-						bc = -1;
-						break;
-					}//if
-					br -= dr[dir];
-					bc -= dc[dir];
-					break;
-				}//if
-			}//while
-			
-			if(rr != -1) map[rr][rc] = EMPTY;
-			if(goal > 1) return null;
-			return new Position(rr, rc, br, bc, cur.cnt+1, cur.dir, Arrays.copyOf(cur.order, 11));
-		}else { // 파란 구슬 먼저 움직임
-			while(true) {
-				br += dr[dir];
-				bc += dc[dir];
-				if(map[br][bc] != EMPTY) {
-					if(map[br][bc] == 'O') {
-						goal++;
-						br = -1;
-						bc = -1;
-						break;
-					}//if
-					br -= dr[dir];
-					bc -= dc[dir];
-					break;
-				}//if
-			}//while
-			
-			if(br != -1) map[br][bc] = 'B';
-			
-			while(true) {
-				rr += dr[dir];
-				rc += dc[dir];
-				if(map[rr][rc] != EMPTY) {
-					if(map[rr][rc] == 'O') {
-						goal++;
-						rr = -1;
-						rc = -1;
-						break;
-					}//if
-					rr -= dr[dir];
-					rc -= dc[dir];
-					break;
-				}
-			}//while			
-			
-			if(br != -1) map[br][bc] = EMPTY;
-			if(goal > 1) return null;		
-			return new Position(rr, rc, br, bc, cur.cnt+1, cur.dir, Arrays.copyOf(cur.order, 11));			
-		}
+		boolean isRed = redFirst(cur, dir);
+		// 빨간 구슬 먼저 움직임	
+		if(isRed) return move(rr, rc, br, bc, 'R', dir, cur, isRed);
+		// 파란 구슬 먼저 움직임	
+		else return move(br, bc, rr, rc, 'B', dir, cur, isRed);	
 
 	}//getNext
 
-	private static boolean getFirst(Position cur, int dir) {
+	private static Position move(int r1, int c1, int r2, int c2, char c, int dir, Position cur, boolean isRed) {
+		int goal = 0;
+		
+		// 첫 번째 구슬 굴림
+		while(true) {
+			r1 += dr[dir];
+			c1 += dc[dir];
+			if(map[r1][c1] != EMPTY) {
+				if(map[r1][c1] == 'O') {
+					goal++;
+					break;
+				}//if
+				r1 -= dr[dir];
+				c1 -= dc[dir];
+				break;
+			}
+		}//while
+		
+		if(isEmpty(r1, c1)) map[r1][c1] = c;
+		
+		// 두 번째 구슬 굴림
+		while(true) {
+			r2 += dr[dir];
+			c2 += dc[dir];
+			if(map[r2][c2] != EMPTY) {
+				if(map[r2][c2] == 'O') {
+					goal++;
+					break;
+				}//if
+				r2 -= dr[dir];
+				c2 -= dc[dir];
+				break;
+			}//if
+		}//while
+		
+		if(isEmpty(r1, c1)) map[r1][c1] = EMPTY;
+		if(goal > 1) return null;
+
+		if(!isRed) return new Position(r2, c2, r1, c1, cur.cnt+1, Arrays.copyOf(cur.order, 11));
+		return new Position(r1, c1, r2, c2, cur.cnt+1, Arrays.copyOf(cur.order, 11));
+	}//move
+
+	// 이동한 칸이 구멍이 아닌 빈칸인지 확인
+	private static boolean isEmpty(int r, int c) {
+		return map[r][c] != 'O';
+	}//isEmpty
+
+	// 빨간 구슬 먼저 움직이는지 확인
+	private static boolean redFirst(Position cur, int dir) {
 		switch (dir) {
 		case U: // 상
 			if(cur.redR > cur.blueR) return false; // 파랑이 더 위에 있음
@@ -202,24 +174,22 @@ public class Main {
 		}//switch
 		
 		return true;
-	}//getFirst
+	}//redFirst
 
 	static class Position {
-		int redR;
-		int redC;
-		int blueR;
-		int blueC;
-		int cnt;
-		int dir;
-		int[] order;
-		public Position(int redR, int redC, int blueR, int blueC, int cnt, int dir, int[] order) {
+		int redR; // 빨간 구슬 행
+		int redC; // 빨간 구슬 열
+		int blueR; // 파란 구슬 행
+		int blueC; // 파란 구슬 열
+		int cnt; // 움직인 횟수
+		int[] order; // 움직인 방법
+		public Position(int redR, int redC, int blueR, int blueC, int cnt, int[] order) {
 			super();
 			this.redR = redR;
 			this.redC = redC;
 			this.blueR = blueR;
 			this.blueC = blueC;
 			this.cnt = cnt;
-			this.dir = dir;
 			this.order = order;
 		}		
 	}//Position
