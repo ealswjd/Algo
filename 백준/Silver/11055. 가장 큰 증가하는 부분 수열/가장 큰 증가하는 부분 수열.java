2fa25f25351417048eb8ea8a6e6f5cd35002ebdd
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-01-16 16:12:02 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 104 ms, Memory: 11880 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/11055
+public class Main {
+	static int N;
+	static int[] arr, dp;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N+1];
+		dp = new int[N+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = arr[i];
+		}//for
+		br.close();
+		
+		int max = dp[1];
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<i; j++) {
+				if(arr[j] < arr[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+					max = Math.max(max, dp[i]);
+				}//if
+			}//for j
+		}//for i
+
+		System.out.print(max);
+	}//main
+
+}//class
