b4224cd7da81ea8dacac1181a68917e458fb7dec
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-01-19 18:09:46 +0900

    [Gold V] Title: 농장 관리, Time: 200 ms, Memory: 77780 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/1245
@@ -14,6 +12,7 @@ public class Main {
 	static int[][] map;
 	static boolean[][] mountain;
 	static List<int[]> list;
+	static boolean isMountain;
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,51 +35,34 @@ public class Main {
 			for(int j=0; j<M; j++) {
 				if(map[i][j] == 0 || mountain[i][j]) continue;
 				list = new ArrayList<>();
+				isMountain = true;
 				dfs(i, j, map[i][j], new boolean[N][M]);
-				if(isMountain()) cnt++;
+				if(isMountain) {
+					cnt++;
+					for(int[] pos : list) {
+						mountain[pos[0]][pos[1]] = true;
+					}
+				}
 			}//for j
 		}//for i
 
 		System.out.print(cnt);
 	}//main
 
-	private static boolean isMountain() {
-		boolean[][] visited = new boolean[N][M];
-		Queue<int[]> q = new LinkedList<>();
-		for(int[] pos : list) {
-			q.offer(pos);
-			visited[pos[0]][pos[1]] = true;
-		}//for
-		
-		int[] cur;
-		int r, c;
-		while(!q.isEmpty()) {
-			cur = q.poll();
-			r = cur[0];
-			c = cur[1];
-			for(int i=0; i<8; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				if(rangeCheck(nr, nc) || visited[nr][nc]) continue;
-				if(map[nr][nc] > map[r][c]) return false;
-			}//for
-		}//while
-		
-		for(int[] pos : list) {
-			mountain[pos[0]][pos[1]] = true;
-		}//for
-		return true;
-	}//isMountain
-
 	private static void dfs(int r, int c, int n, boolean[][] visited) {
+		if(!isMountain) return;
 		list.add(new int[] {r, c});
 		visited[r][c] = true;
 		
 		for(int i=0; i<8; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
-			if(rangeCheck(nr, nc) || visited[nr][nc] || map[nr][nc] != n) continue;
-			dfs(nr, nc, n, visited);
+			if(rangeCheck(nr, nc) || visited[nr][nc]) continue;
+			if(map[nr][nc] > n) {
+				isMountain = false;
+				return;
+			}
+			if(map[nr][nc] == n) dfs(nr, nc, n, visited);
 		}//for
 		
 	}//dfs
