a63b2e7074b8d8d9ca9e072916c5385622a10210
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-01-30 21:17:55 +0900

    [Silver I] Title: 숫자 재배치, Time: 248 ms, Memory: 12224 KB -BaekjoonHub

---
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/16943
+public class Main {
+	static int N, A, B, result;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		int bLen = (int) Math.log10(B) + 1;
+		N = (int)Math.log10(A) + 1;
+		visited = new boolean[N];
+
+		if(bLen < N) System.out.print(-1);
+		else {
+			sol(N, 0);
+			if(result == 0) result = -1;
+			System.out.print(result);			
+		}
+	}//main
+
+	private static void sol(int cnt, int sum) {
+		if(cnt == 0) {
+			if((int)Math.log10(sum) + 1 == N && sum > result && sum < B) result = sum;
+			return;
+		}//if
+		
+		int n = A;
+		for(int i=(int)Math.pow(10, N-1), j=0; j<N; i/=10, j++) {
+			if(visited[j]) continue;
+			n = ((A / i) % 10) * (int)Math.pow(10, cnt-1);
+			visited[j] = true;
+			sol(cnt-1, sum + n);
+			visited[j] = false;
+		}//for
+	}//sol
+
+}//class
