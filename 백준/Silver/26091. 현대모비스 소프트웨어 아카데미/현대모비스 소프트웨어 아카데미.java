33595e31f09fa4b9f4575d904f8a4b35b2ee9b3b
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-02-25 21:49:56 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 344 ms, Memory: 34640 KB -BaekjoonHub

--- 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
@@ -5,28 +5,28 @@ import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/26091
 public class Main {
-    static int N, M;
-    static int[] power;
+    static int N, M; // 학회원의 수, 팀의 최소 능력치
+    static int[] power; // 능력치
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 견학을 희망하는 학회원의 수
+        M = Integer.parseInt(st.nextToken()); // 팀의 최소 능력치
 
-        power = new int[N];
+        power = new int[N]; // 능력치
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
             power[i] = Integer.parseInt(st.nextToken());
         }//for
         br.close();
 
-        int cnt = getCnt();
+        int cnt = getCnt(); // 최대 팀 수
         System.out.print(cnt);
     }//main
 
     private static int getCnt() {
-        if(N < 2) return 0;
+        if(N < 2) return 0; // 조건 : 팀원이 두 명
 
         Arrays.sort(power);
         int cnt = 0;
@@ -38,7 +38,7 @@ public class Main {
             sum = power[start] + power[end];
 
             if(sum < M) start++;
-            else {
+            else { // 능력치 만족
                 cnt++;
                 start++;
                 end--;
