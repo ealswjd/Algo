de8d3cbdb280b292f564765d28ddf2e5fd5384bb
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-03-22 03:19:47 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1292
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int[] sum = new int[B+1];
+        int next = 2;
+        for(int i=1, n=1; i<=B; i++) {
+            if(i == next) {
+                n++;
+                next = n + i;
+            }
+            sum[i] = sum[i-1] + n;
+        }
+
+        System.out.print(sum[B] - sum[A-1]);
+    }//main
+
+}//class
