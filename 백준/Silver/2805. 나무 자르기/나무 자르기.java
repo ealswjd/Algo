08642fe3c8077c41ea00a50171c58dbb5c053fc3
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-03-29 14:19:38 +0900

    [Silver II] Title: 나무 자르기, Time: 728 ms, Memory: 168704 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2805
+public class Main {
+    static int N, M;
+    static int[] trees;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 나무의 수
+        M = Integer.parseInt(st.nextToken()); // 집으로 가져가려고 하는 나무의 길이
+
+        trees = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = getMax();
+        System.out.print(max);
+    }//main
+
+    private static int getMax() {
+        Arrays.sort(trees);
+        int max = 0;
+        int start = 0, end = trees[N-1], mid=(start+end)/2;
+        long sum;
+
+        while(start < end) {
+            mid = (start+end)/2;
+            sum = getSum(mid);
+            if(sum >= M) max = mid;
+
+            if(sum > M) start = mid+1;
+            else if(sum < M ) end = mid;
+            else break;
+        }//while
+
+        return max;
+    }//getMax
+
+    private static long getSum(int mid) {
+        long sum = 0;
+        for(int tree : trees) {
+            if(tree - mid > 0) sum += tree - mid;
+        }
+
+        return sum;
+    }//getSum
+
+}//class
