a29ab6a14ce4de2bf0c6c4a4a5d40c792cb8194f
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-04-05 19:22:03 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 524 ms, Memory: 169380 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/12738
+public class Main {
+    static final int MIN = -1_000_000_001;
+    static int N;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        init();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        int max = getMax();
+        System.out.print(max);
+    }//main
+
+    private static int getMax() {
+        int cnt = 0;
+        int idx;
+
+        for(int i=0; i<N; i++) {
+            if(arr[i] > dp[cnt]) {
+                dp[++cnt] = arr[i];
+                continue;
+            }
+
+            idx = binarySearch(cnt, arr[i]);
+            dp[idx] = arr[i];
+        }//for
+
+        return cnt;
+    }//getMax
+
+    private static int binarySearch(int end, int n) {
+        int start = 0;
+        int mid;
+
+        while(start < end) {
+            mid = (start+end) / 2;
+            if(dp[mid] >= n) end = mid;
+            else start = mid+1;
+        }
+
+        return end;
+    }//binarySearch
+
+    private static void init() {
+        arr = new int[N];
+        dp = new int[N+1];
+        Arrays.fill(dp, MIN);
+    }//init
+
+}//class
