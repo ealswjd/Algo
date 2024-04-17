a386ef0473dc553333fabe78867607125d799962
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-04-17 17:14:18 +0900

    [Bronze III] Title: 최댓값, Time: 76 ms, Memory: 11352 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,30 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-// https://www.acmicpc.net/problem/2562
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[1] - o1[1];
-			}
-		});
-		
-		for(int i=1; i<10; i++) {
-			pq.offer(new int[] {i, Integer.parseInt(br.readLine())});
-		}//for
-		br.close();
-		
-		StringBuilder ans = new StringBuilder();
-		ans.append(pq.peek()[1]).append("\n");
-		ans.append(pq.poll()[0]);
-		System.out.print(ans);
-	}//main
-
-}//class
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+        for (int i = 1; i <= 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int max = arr[1];
+        int idx = 1;
+        for (int i = 2; i <= 9; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                idx = i;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(idx);
+        System.out.println(sb);
+    }
+}
