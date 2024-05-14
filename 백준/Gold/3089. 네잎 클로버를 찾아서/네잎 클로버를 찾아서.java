c5f60b19f8b4ab6bbbe89f6c60a1692aa625ae48
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-05-14 15:29:16 +0900

    [Gold III] Title: 네잎 클로버를 찾아서, Time: 1020 ms, Memory: 54500 KB -BaekjoonHub

--- 백준/Gold/3089. 네잎 클로버를 찾아서/네잎 클로버를 찾아서.java
+++ 백준/Gold/3089. 네잎 클로버를 찾아서/네잎 클로버를 찾아서.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 /**
  * 문제 이름(난이도) : 네잎 클로버를 찾아서 (골드 3)
  * 링크 : https://www.acmicpc.net/problem/3089
+ * 알고리즘 : 이분탐색 
  * */
 public class Main {
     static int N, M;
@@ -17,8 +18,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken()); // 네잎 클로버의 개수
         M = Integer.parseInt(st.nextToken()); // 외계인이 전송한 명령의 수
 
-        X = new Position[N];
-        Y = new Position[N];
+        X = new Position[N]; // x 기준 정렬
+        Y = new Position[N]; // y 기준 정렬
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -32,37 +33,42 @@ public class Main {
     }//main
 
     private static void getXY(char[] orders) {
+        // x 기준 정렬 
         Arrays.sort(X, (pos1, pos2) -> {
             if(pos1.x == pos2.x) return pos1.y - pos2.y;
             return pos1.x - pos2.x;});
+        
+        // y 기준 정렬
         Arrays.sort(Y, (pos1, pos2) -> {
             if(pos1.y == pos2.y) return pos1.x - pos2.x;
             return pos1.y - pos2.y;
         });
 
         int x = 0, y = 0;
-        int idxX, idxY;
+        int idxX, idxY; // X 인덱스, Y 인덱스
 
         for(char c : orders) {
             if(c == 'U' || c == 'D') { // 상하
-                idxX = binarySearchX(x, y);
-                if(c == 'U') idxX++;
-                else idxX--;
+                idxX = binarySearchX(x, y); // 현재 위치 인덱스 찾아서
+                if(c == 'U') idxX++; // 위로 이동
+                else idxX--; // 아래로 이동
 
+                // 위치 갱신
                 x = X[idxX].x;
                 y = X[idxX].y;
 
             }else { // 좌우
-                idxY = binarySearchY(x, y);
-                if(c == 'R') idxY++;
-                else idxY--;
+                idxY = binarySearchY(x, y); // 현재 위치 인덱스 찾아서
+                if(c == 'R') idxY++; // 오른쪽으로 이동
+                else idxY--; // 왼쪽으로 이동
 
+                // 위치 갱신
                 x = Y[idxY].x;
                 y = Y[idxY].y;
             }
         }
 
-        System.out.print(x + " " + y);
+        System.out.print(x + " " + y); // 마지막 위치 출력
     }//getXY
 
     private static int binarySearchX(int x, int y) {
