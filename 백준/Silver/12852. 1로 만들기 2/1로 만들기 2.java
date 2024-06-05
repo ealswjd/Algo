3bdb61620163fbc6a067b6cc1a266904ab56c9b6
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-06-05 15:40:55 +0900

    [Silver I] Title: 1로 만들기 2, Time: 100 ms, Memory: 19476 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -17,7 +17,6 @@ public class Main {
         getResult(cnt);
     }//main
 
-    
     private static void getResult(int cnt) {
         StringBuilder ans = new StringBuilder();
         ans.append(cnt).append('\n');
@@ -30,20 +29,19 @@ public class Main {
         System.out.print(ans);
     }//getResult
 
-    
     private static int getMinCnt() {
+
         for(int i=2; i<=N; i++) {
             dp[i] = dp[i-1] + 1;
-            numbers[i] = i-1;
+            numbers[i] = i - 1;
 
+            if(i % 3 == 0 && dp[i/3]+1 < dp[i]) {
+                dp[i] = dp[i/3] + 1;
+                numbers[i] = i / 3;
+            }
             if(i % 2 == 0 && dp[i/2] + 1 < dp[i]) {
                 dp[i] = dp[i/2] + 1;
-                numbers[i] = i/2;
-            }
-
-            if(i % 3 == 0 && dp[i/3] + 1 < dp[i]) {
-                dp[i] = dp[i/3] + 1;
-                numbers[i] = i/3;
+                numbers[i] = i / 2;
             }
         }
 
