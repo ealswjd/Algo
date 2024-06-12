2455576b74dcb18a33c494eaebf7762685e64d1e
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-06-12 18:30:19 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 140 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -49,10 +49,10 @@ public class Main {
             int dist = tmp[1];
 
             for(int next : list.get(cur)) {
-                if (minArr[next] == -1) {
-                    minArr[next] = dist + 1;
-                    q.offer(new int[]{next, dist + 1});
-                }
+                if (minArr[next] != -1) continue;
+                
+                minArr[next] = dist + 1;
+                q.offer(new int[]{next, dist + 1});
             }
         }
 
@@ -65,51 +65,52 @@ public class Main {
 
     private static void getCycle() {
         boolean[] visited = new boolean[N+1];
-        parent = new int[N+1];
-        Arrays.fill(parent, -1);
 
         for(int i = 1; i <= N; i++) {
-            if(!visited[i]) {
-                if(dfs(i, -1, visited)) {
-                    break;
-                }
-            }
+            if(visited[i]) continue;
+            if(dfs(i, -1, visited)) break;
         }
+        
     }//getCycle
 
     private static boolean dfs(int cur, int par, boolean[] visited) {
         visited[cur] = true;
+
         for (int next : list.get(cur)) {
             if (next == par) continue; // 부모 노드로 돌아가는 것 방지
             if (visited[next]) {
                 markCycle(cur, next);
                 return true;
             }
+            
             parent[next] = cur;
             if (dfs(next, cur, visited)) {
                 return true;
             }
         }
+        
         return false;
     }//dfs
 
     private static void markCycle(int start, int end) {
-        Arrays.fill(isCycle, false);
-        
+
         while (start != end) {
             isCycle[start] = true;
             start = parent[start];
         }
-        
+
         isCycle[end] = true;
     }//markCycle
 
     private static void init() {
         list = new ArrayList<>(N+1); // 역과 역을 연결하는 구간의 정보
         isCycle = new boolean[N+1];
+        parent = new int[N+1];
+        Arrays.fill(parent, -1);
+
         for (int i = 0; i <= N; i++) {
             list.add(new ArrayList<>());
         }
     }//init
-    
+
 }//class
