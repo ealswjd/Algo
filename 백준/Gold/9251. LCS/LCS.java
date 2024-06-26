4a6ac68a8ee63b67b1ade67219cab87f952e2eee
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-06-26 22:32:38 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,41 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// https://www.acmicpc.net/problem/9251
 public class Main {
-    static int lenA, lenB;
     static char[] A, B;
-    static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         A = br.readLine().toCharArray();
         B = br.readLine().toCharArray();
-        br.close();
 
-        init();
-
-        int length = getLength();
-        System.out.print(length);
+        int len = getLength();
+        System.out.println(len);
     }//main
 
     private static int getLength() {
+        int aLen = A.length;
+        int bLen = B.length;
+        int[][] dp = new int[aLen+1][bLen+1];
 
-        for(int a=1; a<=lenA; a++) {
-            for(int b=1; b<=lenB; b++) {
+        for(int a=1; a<=aLen; a++) {
+            for(int b=1; b<=bLen; b++) {
                 if(A[a-1] == B[b-1]) dp[a][b] = dp[a-1][b-1] + 1;
-                else {
-                    dp[a][b] = Math.max(dp[a-1][b], dp[a][b-1]);
-                }
+                else dp[a][b] = Math.max(dp[a-1][b], dp[a][b-1]);
             }
         }
 
-        return dp[lenA][lenB];
-    }//getMinCnt
-
-    private static void init() {
-        lenA = A.length;
-        lenB = B.length;
-        dp = new int[lenA+1][lenB+1];
-    }//init
+        return dp[aLen][bLen];
+    }//getLength
 
 }//class
