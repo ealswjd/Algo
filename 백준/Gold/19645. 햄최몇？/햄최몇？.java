128d9fd19f8da99de5725119b00c94341f2faadc
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-07-13 15:51:51 +0900

    [Gold I] Title: 햄최몇?, Time: 188 ms, Memory: 37748 KB -BaekjoonHub

--- 백준/Gold/19645. 햄최몇？/햄최몇？.java
+++ 백준/Gold/19645. 햄최몇？/햄최몇？.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/19645
 public class Main {
     static int N; // 햄버거의 수
     static int[] V; // 햄버거의 효용
+    static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,49 +20,41 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int total = 0; // 효용 총합
-        for(int i=1; i<=N; i++) {
+        for(int i=0; i<N; i++) {
             V[i] = Integer.parseInt(st.nextToken());
             total += V[i];
         }
 
-        int max = getMax(total); // 막내 길원이가 얻을 수 있는 효용의 합의 최댓값
+        dp = new int[total+1][total+1];
+        for(int i=0; i<=total; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int max = getMax(0, 0, 0, total); // 막내 길원이가 얻을 수 있는 효용의 합의 최댓값
         bw.write(String.valueOf(max));
         bw.flush();
         bw.close();
         br.close();
     }//main
 
-    private static int getMax(int total) {
-        int[][] dp = new int[total+1][total+1];
-        dp[0][0] = 1;
-
-        for(int i=1; i<=N; i++) {
-            for(int a=total; a>=0; a--) {
-                for(int b=total; b>=0; b--) {
-                    if(a - V[i] >= 0) {
-                        dp[a][b] |= dp[a- V[i]][b];
-                    }
-                    if(b - V[i] >= 0) {
-                        dp[a][b] |= dp[a][b- V[i]];
-                    }
-                }
-            }
+    private static int getMax(int n, int a, int b, int total) {
+        if(n == N) {
+            int k =  total - a - b;
+            if(a >= k && b >= k) return k;
+            return 0;
         }
+        if(dp[a][b] != -1) return dp[a][b];
 
         int max = 0;
-        for(int a=0; a<=total; a++) {
-            for(int b=0; b<=a; b++) {
-                if(dp[a][b] == 1 && b >= (total - a - b)) {
-                    max = Math.max(max, total - a - b);
-                }
-            }
-        }
+        if(a + V[n] <= total) max = Math.max(max, getMax(n+1, a+V[n], b, total));
+        if(b + V[n] <= total) max = Math.max(max, getMax(n+1, a, b+V[n], total));
+        max = Math.max(max, getMax(n+1, a, b, total));
 
-        return max;
+        return dp[a][b] = max;
     }//getMax
 
     private static void init() {
-        V = new int[N+1]; // 햄버거의 효용
+        V = new int[N];
     }//init
 
 }//class
