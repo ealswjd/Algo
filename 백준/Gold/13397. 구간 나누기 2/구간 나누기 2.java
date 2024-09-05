6d8f853202e5f79fe8f8900208686aabef0daf81
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-09-05 16:05:56 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 96 ms, Memory: 12612 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -10,44 +10,49 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 배열의 크기
+        M = Integer.parseInt(st.nextToken()); // M개 이하의 구간
 
-        arr = new int[N];
         int max = 0;
+        arr = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, arr[i]);
         }
 
-        int ans = getResult(max);
-        System.out.print(ans);
+        int min = getMin(max);
+        System.out.print(min);
     }//main
 
-    private static int getResult(int max) {
-        int min = 0;
+    
+    private static int getMin(int max) {
+        int start = 0;
+        int end = max;
         int mid;
 
-        while(min < max) {
-            mid = (min+max) / 2;
-            if(getCnt(mid) <= M) max = mid;
-            else min = mid+1;
+        while(start < end) {
+            mid = (start + end) / 2;
+
+            if(getCnt(mid) <= M) end = mid;
+            else start = mid + 1;
         }
 
-        return max;
-    }//getMax
+        return end;
+    }//getMin
 
-    private static int getCnt(int mid) {
+    
+    private static int getCnt(int target) {
         int cnt = 1;
-        int max = arr[0];
         int min = arr[0];
+        int max = arr[0];
 
         for(int i=0; i<N; i++) {
-            min = Math.min(min, arr[i]);
-            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]); // 최솟값
+            max = Math.max(max, arr[i]); // 최댓값
 
-            if(max-min > mid) {
+            if(max - min > target) {
                 if(++cnt > M) return cnt;
                 min = arr[i];
                 max = arr[i];
@@ -57,4 +62,5 @@ public class Main {
         return cnt;
     }//getCnt
 
+    
 }//class
