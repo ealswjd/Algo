31bd9e17bc50b4b0b836535f694e33230c92531a
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-09-29 22:38:33 +0900

    [Silver IV] Title: 알고리즘 수업 - 행렬 경로 문제 1, Time: 60 ms, Memory: 11416 KB -BaekjoonHub

--- 백준/Silver/24418. 알고리즘 수업 － 행렬 경로 문제 1/알고리즘 수업 － 행렬 경로 문제 1.java
+++ 백준/Silver/24418. 알고리즘 수업 － 행렬 경로 문제 1/알고리즘 수업 － 행렬 경로 문제 1.java
@@ -1,38 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/24418
 public class Main {
     static int N;
-    static int cnt;
-    static int[][] map;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
 
-        StringTokenizer st;
         for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            br.readLine();
+        }
+
+        int cnt = getCnt();
+        System.out.print(cnt + " " + N*N);
+    }//main
+
+    private static int getCnt() {
+        int cnt = 1;
+        int[][] dp = new int[N+1][N+1];
+        dp[1][1] = 1;
+
+        for(int r=1; r<=N; r++) {
+            for(int c=1; c<=N; c++) {
+                if(r != 1 || c != 1) dp[r][c] = dp[r-1][c] + dp[r][c-1];
+                cnt += dp[r][c];
             }
         }
 
-        getMax(N-1, N-1);
+        return cnt;
+    }//getCnt
 
-        System.out.println(cnt+1 + " " + N*N);
-    }//main
 
-    
-    private static int getMax(int r, int c) {
-        if(r < 0 || c < 0) return 0;
-
-        cnt++;
-        return map[r][c] + Math.max(getMax(r-1, c), getMax(r, c-1));
-    }//getMax
-
-    
 }//class
