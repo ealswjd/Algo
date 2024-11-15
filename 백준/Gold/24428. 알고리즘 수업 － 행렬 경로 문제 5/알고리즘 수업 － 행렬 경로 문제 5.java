272860516b4a5b00f85d4158d95a6312061eaaa0
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-11-15 17:12:59 +0900

    [Gold IV] Title: 알고리즘 수업 - 행렬 경로 문제 5, Time: 524 ms, Memory: 139072 KB -BaekjoonHub

--- 백준/Gold/24428. 알고리즘 수업 － 행렬 경로 문제 5/알고리즘 수업 － 행렬 경로 문제 5.java
+++ 백준/Gold/24428. 알고리즘 수업 － 행렬 경로 문제 5/알고리즘 수업 － 행렬 경로 문제 5.java
@@ -1,20 +1,64 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/24428
 public class Main {
-    static int N, pCnt;
-    static int[][] map;
-    static int[][][] dp;
-    static boolean[][] isP;
+    private static int N; // 행렬의 크기
+    private static int[][] map;
+    private static boolean[][] isP; // 중간 원소 여부
 
+    
+    public static void main(String[] args) throws IOException {
+        init();
 
-    public static void main(String[] args) throws Exception {
+        int max = getMax();
+        System.out.print(max);
+    }//main
+
+    
+    private static int getMax() {
+        int P = 3; // 적어도 세 개의 원소를 반드시 거쳐야 함
+        int[][][] dp = new int[N+1][N+1][P+1]; // 0 ~ 3개를 거쳤을 때 가장 높은 점수
+        dp[N][N][P] = -1; // 도착 여부를 확인하기 위해 -1로 초기화
+        dp[1][1][0] = map[1][1]; // 시작점 초기화
+
+        int[] fromTop;
+        int[] fromLeft;
+
+        for(int r=1; r<=N; r++) {
+            for(int c=1; c<=N; c++) {
+                if(r == 1 && c == 1) continue;
+
+                fromTop = dp[r-1][c]; // 위
+                fromLeft = dp[r][c-1]; // 왼쪽
+
+                // 중간 원소를 0~P개 거쳤을 때 최고 점수 갱신
+                for(int p=0; p<=P; p++) {
+                    int max = Math.max(fromTop[p], fromLeft[p]);
+                    if(max > 0) dp[r][c][p] = max + map[r][c];
+                }
+
+                // 현재 원소가 중간 원소임
+                if(isP[r][c]) {
+                    for(int p=P-1; p>=0; p--) {
+                        int max = Math.max(fromTop[p], fromLeft[p]);
+                        if(max > 0) dp[r][c][p+1] = max + map[r][c];
+                    }
+                }
+            }
+        }
+
+        return dp[N][N][P];
+    }//getMax
+
+    
+    private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
-        init();
+        map = new int[N+1][N+1];
+        isP = new boolean[N+1][N+1];
 
         StringTokenizer st;
         for(int i=1; i<=N; i++) {
@@ -24,62 +68,17 @@ public class Main {
             }
         }
 
-        pCnt = Integer.parseInt(br.readLine());
-        for(int i=0; i<pCnt; i++) {
+        // P개의 중간 원소
+        int P = Integer.parseInt(br.readLine());
+        for(int i=0; i<P; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
             isP[r][c] = true;
         }
 
-        int maxScore = getMaxScore();
-        System.out.print(maxScore);
-    }//main
-
-
-    private static int getMaxScore() {
-
-        for(int r=1; r<=N; r++) {
-            for(int c=1; c<=N; c++) {
-                dp[r][c][0] = Math.max(dp[r-1][c][0], dp[r][c-1][0]) + map[r][c];
-
-                int[] fromTop = dp[r-1][c];
-                int[] fromLeft = dp[r][c-1];
-
-                int max = Math.max(fromTop[1], fromLeft[1]);
-                if(max > 0) dp[r][c][1] = max + map[r][c];
-
-                max = Math.max(fromTop[2], fromLeft[2]);
-                if(max > 0) dp[r][c][2] = max + map[r][c];
-
-                max = Math.max(fromTop[3], fromLeft[3]);
-                if(max > 0) dp[r][c][3] = max + map[r][c];
-
-                // 중간 원소인 경우
-                if(isP[r][c]) {
-                    max = Math.max(fromTop[2], fromLeft[2]);
-                    if(max > 0) dp[r][c][3] = Math.max(dp[r][c][3], max + map[r][c]);
-
-                    max = Math.max(fromTop[1], fromLeft[1]);
-                    if(max > 0) dp[r][c][2] = Math.max(dp[r][c][2], max + map[r][c]);
-
-                    max = Math.max(fromTop[0], fromLeft[0]);
-                    if(max > 0) dp[r][c][1] = Math.max(dp[r][c][1], max + map[r][c]);
-                }
-            }
-        }
-
-
-        return dp[N][N][3] > 0 ? dp[N][N][3] : -1;
-    }//getMaxScore
-
-
-    private static void init() {
-        map = new int[N+1][N+1];
-        dp = new int[N+1][N+1][4];
-        isP = new boolean[N+1][N+1];
+        br.close();
     }//init
 
-
+    
 }//class
