74a12cfd56abda7e5fe03ec2b31d2fb542d3cd37
minjeong <79951187+ealswjd@users.noreply.github.com>
2024-11-27 19:25:16 +0900

    [Gold II] Title: 미확인 도착지, Time: 496 ms, Memory: 63848 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -9,8 +9,7 @@ public class Main {
     private static int N; // 교차로 개수
     private static int S, G, H; // 예술가들의 출발지, 지나간 도로(G -> H)
     private static List<List<int[]>> list; // 도로 정보
-    private static boolean[] isCandidate; // 목적지 후보
-    private static boolean[] isTarget; // 목적지
+    private static int[] candidate; // 목적지 후보
 
 
     public static void main(String[] args) throws IOException {
@@ -28,11 +27,20 @@ public class Main {
 
 
     private static void getTarget(StringBuilder ans) {
+        int[] sCost = dijkstra(S);
+        int[] gCost = dijkstra(G);
+        int[] hCost = dijkstra(H);
+
+        boolean[] isTarget = new boolean[N+1]; // 목적지
+
         // 가능한 목적지 탐색
-        for(int i=1; i<=N; i++) {
-            if(isCandidate[i]) {
-                if(dijkstra(i)) isTarget[i] = true;
-            }
+        for(int city : candidate) {
+            int gToH = sCost[G] + gCost[H] + hCost[city];
+            int hToG = sCost[H] + hCost[G] + gCost[city];
+
+            int min = Math.min(gToH, hToG);
+
+            if(min == sCost[city]) isTarget[city] = true;
         }
 
         // 가능한 목적지들을 공백으로 분리시켜 오름차순
@@ -44,48 +52,30 @@ public class Main {
     }//getTarget
 
 
-    private static boolean dijkstra(int target) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if(o1[1] == o2[1]) return o2[2] - o1[2];
-            return o1[1] - o2[1];
-        });
+    private static int[] dijkstra(int start) {
+        Queue<Integer> q = new LinkedList<>();
         int[] minCost = new int[N+1];
         Arrays.fill(minCost, INF);
-        minCost[S] = 0;
+        minCost[start] = 0;
 
-        int visited = 0;
-        pq.offer(new int[] {S, 0, visited});
+        q.offer(start);
 
-        while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int node = cur[0];
-            int dist = cur[1];
-            visited = cur[2];
+        while(!q.isEmpty()) {
+            int cur = q.poll();
 
-            if(minCost[node] < dist) continue;
-            if(node == target) return visited == 3;
-
-            for(int[] next : list.get(node)) {
+            for(int[] next : list.get(cur)) {
                 int nextNode = next[0];
-                int nextDist = dist + next[1];
-                int nextVisited = visited;
-
-                if(isRoadPassed(node, nextNode)) nextVisited = 3;
+                int nextDist = minCost[cur] + next[1];
 
                 if(minCost[nextNode] > nextDist) {
                     minCost[nextNode] = nextDist;
-                    pq.offer(new int[] {nextNode, nextDist, nextVisited});
+                    q.offer(nextNode);
                 }
             }
         }
 
-        return false;
+        return minCost;
     }//dijkstra
-
-
-    private static boolean isRoadPassed(int a, int b) {
-        return (a == G && b == H) || (a == H && b == G);
-    }//isRoadPassed
 
 
     private static void init(BufferedReader br) throws IOException {
@@ -101,8 +91,7 @@ public class Main {
         G = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
 
-        isCandidate = new boolean[N+1]; // 목적지 후보
-        isTarget = new boolean[N+1]; // 목적지
+        candidate = new int[T]; // 목적지 후보
         list = new ArrayList<>(N+1); // 도로 정보
 
         for(int i=0; i<=N; i++) {
@@ -115,18 +104,16 @@ public class Main {
             // a와 b 사이에 길이 d의 양방향 도로가 있다는 뜻
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken()) * 2;
-
-            if(isRoadPassed(a, b)) d--;
+            int d = Integer.parseInt(st.nextToken());
 
             list.get(a).add(new int[] {b, d});
             list.get(b).add(new int[] {a, d});
         }
 
         // T개의 각 줄마다 정수 x가 주어지는데, T개의 목적지 후보들을 의미
-        while(T-- > 0) {
+        for(int i=0; i<T; i++) {
             int x = Integer.parseInt(br.readLine());
-            isCandidate[x] = true;
+            candidate[i] = x;
         }
 
     }//init
