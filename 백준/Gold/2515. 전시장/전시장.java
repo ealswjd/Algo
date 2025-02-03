8eeae1b3b2a3303257f1a991c8835bc57722d456
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-02-03 18:37:32 +0900

    [Gold II] Title: 전시장, Time: 428 ms, Memory: 158064 KB -BaekjoonHub

--- 백준/Gold/2515. 전시장/전시장.java
+++ 백준/Gold/2515. 전시장/전시장.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/2515
 public class Main {
+    private static final int H = 20_000_001;
     private static int N; // 그림의 개수
     private static int S; // 판매가능 그림 높이 기준
-    private static Picture[] pictures; // 그림
+    private static int maxH;
+    private static int[] maxCost;
 
 
     public static void main(String[] args) throws IOException {
@@ -20,40 +21,12 @@ public class Main {
 
 
     private static int getMaxCost() {
-        int[] dp = new int[N];
-        dp[0] = pictures[0].cost;
-
-        for(int i=1; i<N; i++) {
-            int maxIndex = getMaxIndex(i);
-
-            // 탐색 x
-            if(maxIndex == -1) {
-                dp[i] = Math.max(dp[i-1], pictures[i].cost);
-            }
-            else {
-                dp[i] = Math.max(dp[i-1], dp[maxIndex] + pictures[i].cost);
-            }
+        for(int i=S; i<=maxH; i++) {
+            maxCost[i] = Math.max(maxCost[i-1], maxCost[i - S] + maxCost[i]);
         }
 
-        return dp[N-1];
+        return maxCost[maxH];
     }//getMaxCost
-
-
-    private static int getMaxIndex(int cur) {
-        int start = 0;
-        int end = cur;
-        int mid, diff;
-
-        while(start <= end) {
-            mid = (start + end) / 2;
-            diff = pictures[cur].height - pictures[mid].height;
-
-            if(diff >= S) start = mid + 1;
-            else end = mid - 1;
-        }
-
-        return end;
-    }//getMaxIndex
 
 
     private static void init() throws IOException {
@@ -62,35 +35,19 @@ public class Main {
         N = Integer.parseInt(st.nextToken()); // 그림의 개수
         S = Integer.parseInt(st.nextToken()); // 판매가능 그림 높이 기준
 
-        pictures = new Picture[N];
+        maxCost = new int[H];
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             int height = Integer.parseInt(st.nextToken()); // 높이
             int cost = Integer.parseInt(st.nextToken()); // 가격
 
-            pictures[i] = new Picture(height, cost);
+            maxH = Math.max(maxH, height);
+            maxCost[height] = Math.max(maxCost[height], cost);
         }
 
-        Arrays.sort(pictures);
         br.close();
     }//init
 
 
-    private static class Picture implements Comparable<Picture>{
-        int height;
-        int cost;
-
-        public Picture(int height, int cost) {
-            this.height = height;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Picture o) {
-            return this.height - o.height;
-        }
-    }//Picture
-
-    
 }//class
