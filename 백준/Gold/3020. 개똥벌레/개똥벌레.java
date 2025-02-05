a2602d811edd5a0456d357d4a8571ff74b0d6628
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-02-05 18:41:18 +0900

    [Gold V] Title: 개똥벌레, Time: 236 ms, Memory: 32552 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,62 +1,76 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
-// https://www.acmicpc.net/problem/3020
+/*
+ https://www.acmicpc.net/problem/3020
+ 개똥벌레가 파괴해야하는 장애물의 최솟값과 그러한 구간이 총 몇 개 있는지 구하는 프로그램
+ */
 public class Main {
-	static int N, H;
-	static int[] cnts;
+    private static final int MAX = 200_000;
+    private static int N; // 동굴의 길이
+    private static int H; // 동굴의 높이
+    private static int[] top; // 종유석
+    private static int[] bottom; // 석순
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 동굴 길이
-		H = Integer.parseInt(st.nextToken()); // 동굴 높이
-		int[] top = new int[H+1];
-		int[] bottom = new int[H+1];
-		cnts = new int[H];
-		
-		for(int i=1,h=0; i<=N; i++) {
-			h = Integer.parseInt(br.readLine()); // 높이
-			
-			if(i%2!=0) { // 석순
-				bottom[1]++;
-				bottom[h+1]--;
-			}else { // 종유석
-				top[1]++;
-				top[h+1]--;
-			}
-		}//for
-		br.close();
-		
-		for(int i=1; i<=H; i++) {
-			top[i] += top[i-1];
-			bottom[i] += bottom[i-1];
-		}//for
-		
-		for(int i=1; i<=H; i++) {
-			cnts[i-1] += bottom[i] + top[H-i+1];
-		}//for
-		
-		getResult();
-	}//main
 
-	private static void getResult() {
-		Arrays.sort(cnts);
-		int min = cnts[0];
-		int start=0, end=H, mid = (start+end)/2;
-		
-		while(start<=end) {
-			mid = (start+end)/2;
-			if(cnts[mid] > min) end = mid-1;
-			else if(cnts[mid] <= min) start = mid+1;
-		}//while				
-		
-		if(cnts[mid]==min) mid++;
-		StringBuilder ans = new StringBuilder();
-		ans.append(min).append(" ").append(mid);
-		System.out.print(ans);
-	}//getResult
+    public static void main(String[] args) throws IOException {
+        init();
+        solution();
+    }//main
+
+
+    private static void solution() {
+        int[] count = new int[H]; // 구간별 장애물 개수
+        int min = MAX; // 파괴해야하는 장애물의 최솟값
+        int cnt = 0;   // 그러한 구간 개수
+
+        for(int i=1, j=0; i<=H; i++, j++) {
+            count[j] = bottom[i] + top[H - i + 1];
+
+            if(min > count[j]) {
+                min = count[j];
+                cnt = 1;
+            }
+            else if(min == count[j]) {
+                cnt++;
+            }
+        }
+
+        System.out.printf("%d %d", min, cnt);
+    }//solution
+
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 동굴의 길이
+        H = Integer.parseInt(st.nextToken()); // 동굴의 높이
+
+        top = new int[H + 1]; // 종유석
+        bottom = new int[H + 1]; // 석순
+
+        for(int i=1; i<=N; i++) {
+            int h = Integer.parseInt(br.readLine());
+
+            // 석순
+            if(i % 2 == 1) {
+                bottom[1]++;
+                bottom[h + 1]--;
+            }
+            // 종유석
+            else {
+                top[1]++;
+                top[h + 1]--;
+            }
+        }
+
+        for(int i=1; i<=H; i++) {
+            top[i] += top[i-1];
+            bottom[i] += bottom[i-1];
+        }
+
+        br.close();
+    }//init
+
 
 }//class
