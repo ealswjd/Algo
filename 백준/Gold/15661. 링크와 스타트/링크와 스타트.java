85a886cd633eb8df50550d4044370f32157d088f
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-03-04 16:20:57 +0900

    [Gold V] Title: 링크와 스타트, Time: 84 ms, Memory: 11832 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -6,8 +6,9 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/15661
 public class Main {
     private static int N; // 인원수
-    private static int total; // 총 인원 선택 비트
-    private static int[][] score; // i와 j가 같은 팀에 속했을 때 팀에 더해지는 능력치
+    private static int totalScore; // 총 점수
+    private static int[] row;
+    private static int[] col;
     private static int min; // 능력치의 차이의 최솟값
 
 
@@ -15,62 +16,38 @@ public class Main {
         init();
 
         // 스타트 팀과 링크 팀의 능력치의 차이의 최솟값을 출력
-        getMin(0, 0);
+        getMin(0, 0, totalScore);
         System.out.print(min);
     }//main
 
 
-    private static void getMin(int cur, int checked) {
-        if(1 <= checked && checked < total) {
-            min = Math.min(min, getDiff(checked));
+    private static void getMin(int cur, int cnt, int total) {
+        if(cur == N || cnt == N / 2) {
+            min = Math.min(min, Math.abs(total));
+            return;
         }
 
-        for(int i=cur; i<N; i++) {
-            getMin(i + 1, checked | (1 << i));
-        }
+        getMin(cur + 1, cnt + 1, total - row[cur] - col[cur]);
+        getMin(cur + 1, cnt, total);
     }//getMin
-
-
-    private static int getDiff(int checked) {
-        int start = 0; // 스타트팀 점수
-        int link = 0;  // 링크팀 점수
-        boolean isStartI, isStartJ;
-
-        for(int i=0; i<N; i++) {
-            isStartI = (checked & (1 << i)) != 0; // i가 스타트팀
-
-            for(int j=0; j<N; j++) {
-                isStartJ = (checked & (1 << j)) != 0; // j가 스타트팀
-
-                // 스타트팀
-                if(isStartI && isStartJ) {
-                    start += score[i][j];
-                }
-                // 링크팀
-                else if(!isStartI && !isStartJ) {
-                    link += score[i][j];
-                }
-            }
-        }
-
-        // 능력차이
-        return Math.abs(start - link);
-    }//getDiff
 
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine()); // 인원수
 
-        score = new int[N][N];   // i와 j가 같은 팀에 속했을 때 팀에 더해지는 능력치
         min = Integer.MAX_VALUE; // 능력치의 차이의 최솟값
-        total = (1 << N) - 1;    // 총 인원 선택 비트
+        row = new int[N];
+        col = new int[N];
 
         StringTokenizer st;
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
-                score[i][j] = Integer.parseInt(st.nextToken());
+                int score = Integer.parseInt(st.nextToken());
+                row[i] += score;
+                col[j] += score;
+                totalScore += score;
             }
         }
 
