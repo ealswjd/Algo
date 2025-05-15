4a3993eb13ebc20380c4eb729de98c4dd4fc23d9
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-05-15 16:24:15 +0900

    [Gold I] Title: K번째 수, Time: 84 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,35 +1,59 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 // https://www.acmicpc.net/problem/1300
 public class Main {
-	static int N, K;
+    private static int N, K; // 크기가 N×N인 배열, 1차원 배열에서 구해야 하는 인덱스 K
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		K = Integer.parseInt(br.readLine());
-		br.close();
-		
-		int num = getK();
-		System.out.print(num);
-	}//main
 
-	private static int getK() {
-		int start = 1, end = K, mid;
-		long cnt;
-		
-		while(start <= end) {
-			mid = (start + end)/2;
-			cnt = 0;
-			for(int i=1; i<=N; i++) {
-				cnt += Math.min(mid/i, N);
-			}//for
-			if(cnt < K) start = mid+1;
-			else end = mid-1;
-		}//while
-		
-		return start;
-	}//getK
+    public static void main(String[] args) throws IOException {
+        sol();
+    }//main
+
+
+    private static void sol() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 크기가 N×N인 배열 A
+        K = Integer.parseInt(br.readLine()); // 1차원 배열에서 구해야 하는 인덱스
+        br.close();
+
+        long ans = getK();
+        System.out.print(ans);
+    }//sol
+
+
+    private static long getK() {
+        int start = 1;
+        int end = K;
+        int number = K;
+        int mid;
+
+        while(start <= end) {
+            mid = (start + end) / 2;
+
+            if(getCount(mid) >= K) {
+                end = mid - 1;
+                number = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return number;
+    }//getK
+
+
+    private static long getCount(int mid) {
+        long cnt = 0; // mid 보다 작거나 같은 수의 개수
+
+        for(int n=1; n<=N; n++) {
+            cnt += Math.min(mid / n, N);
+        }
+
+        return cnt;
+    }//getCount
+
 
 }//class
