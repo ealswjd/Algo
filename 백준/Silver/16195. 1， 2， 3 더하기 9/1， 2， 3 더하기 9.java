20e5bc920c9ec73f47bc20931db2a3a8667ae86c
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-06-01 15:34:42 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 140 ms, Memory: 22068 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/16195
+public class Main {
+    private static final int MOD = 1_000_000_009; // 방법의 수를 1,000,000,009로 나눈 나머지
+    private static final int N = 1_000;
+    private static long[][] dp;
+
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+    }//main
+
+
+    private static void sol() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int n, m;
+        long sum;
+
+        while(T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken()); // n을 1, 2, 3의 합으로 나타내는 방법
+            m = Integer.parseInt(st.nextToken()); // 사용한 수의 개수는 m개 이하
+            sum = 0;
+
+            for(int i=1; i<=m; i++) {
+                sum = (sum + dp[n][i]) % MOD;
+            }
+
+            ans.append(sum).append('\n');
+        }
+
+        br.close();
+        System.out.print(ans);
+    }//sol
+
+
+    private static void init() {
+        dp = new long[N+1][N+1];
+
+        dp[1][1] = 1;
+        dp[2][1] = 1; dp[2][2] = 1;
+        dp[3][1] = 1; dp[3][2] = 2; dp[3][3] = 1;
+
+        // m개로 만들 수 있는 방법
+        for(int n=4; n<=N; n++) {
+            for(int m=2; m<=N; m++) {
+                dp[n][m] = (dp[n-1][m-1] + dp[n-2][m-1] + dp[n-3][m-1]) % MOD;
+            }
+        }
+
+    }//init
+
+
+}//class
