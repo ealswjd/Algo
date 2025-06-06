ed1dd57abea0db5c0fb230cee4eef54ecfdb93de
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-06-07 06:57:49 +0900

    [level 2] Title: 문자열 압축, Time: 2.20 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,11 +1,14 @@
 class Solution {
+    
     public int solution(String s) {
         int min = s.length(); // 문자열 s의 길이로 초기화
         int len = min/2; // 길이의 반
+        
         for(int i = 1; i <= len; i++) {
             String prev = ""; // 이전 문자열
             int sum = 0; // 문자열 길이
             int cnt = 1; // 반복되는 문자 수
+            
             for(int j = 0; j < s.length();) {               
                 int start = j;
                 j = (j+i > s.length()) ? s.length() : j+i;
@@ -20,15 +23,16 @@ class Solution {
                     prev = now; 
                     cnt = 1; 
                 }
-            }// for j
+            }
             
             sum += prev.length(); // 이전 문자열의 길이만큼 더해주기
             if(cnt != 1) { // cnt가 1이 아닐 경우
                 sum += (int)Math.log10(cnt)+1; // cnt의 자릿수만큼 더해주기
             }
             min = (min > sum) ? sum : min; 
-        }// for i
+        }
 
         return min; // 가장 짧은 길이 return
     }
+    
 }
