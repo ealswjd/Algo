25293ad44172c3f50a89f782eaca4e281ae27a08
minjeong <79951187+ealswjd@users.noreply.github.com>
2025-11-11 22:35:18 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 724 ms, Memory: 254360 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/10816
 public class Main {
+    private static final int MAX = 10_000_000;   
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다.
         int N = Integer.parseInt(br.readLine()); // 숫자 카드의 개수
-        Map<Integer, Integer> count = new HashMap<>();
+        int[] count = new int[MAX * 2 + 1];
 
         // 숫자 카드에 적혀있는 정수가 주어진다
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            count.put(num, count.getOrDefault(num, 0) + 1);
+            int num = Integer.parseInt(st.nextToken()) + MAX;
+            count[num]++;
         }
 
         // M(1 ≤ M ≤ 500,000)이 주어진다.
@@ -28,8 +27,8 @@ public class Main {
         // 구해야 할 M개의 정수가 주어진다.
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            ans.append(count.getOrDefault(target, 0)).append(' ');
+            int target = Integer.parseInt(st.nextToken()) + MAX;
+            ans.append(count[target]).append(' ');
         }
 
         br.close();
