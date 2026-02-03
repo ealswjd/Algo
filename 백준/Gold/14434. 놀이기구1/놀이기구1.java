802ae6c4eda1e1bfd57fd2ba27b06286af2f0b3c
minjeong <79951187+ealswjd@users.noreply.github.com>
2026-02-03 15:41:49 +0900

    [Gold I] Title: 놀이기구1, Time: 820 ms, Memory: 135176 KB -BaekjoonHub

--- 백준/Gold/14434. 놀이기구1/놀이기구1.java
+++ 백준/Gold/14434. 놀이기구1/놀이기구1.java
@@ -7,105 +7,110 @@ import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/14434
 public class Main {
-    static int N, M, K, Q;
-    static int[] limits; // 놀이기구들의 키제한
-    static int[] days; // 각 날마다 놀이기구 타는 횟수
-    static List<List<Integer>> growList; // 키 성장 정보
+    private static int K;
+    private static int[] days; // 날마다 아이들이 놀이기구 타는 횟수
+    private static List<List<Integer>> height; // 아이들 키 정보
 
     public static void main(String[] args) throws IOException {
-        init();
+        sol();
         print();
     }//main
-
 
     private static void print() {
         StringBuilder ans = new StringBuilder();
 
-        for(int d=1; d<=K; d++) {
-            days[d] += days[d-1];
-            ans.append(days[d]).append('\n');
+        for(int day=1; day<=K; day++) {
+            days[day] += days[day-1];
+            ans.append(days[day]).append('\n');
         }
 
         System.out.print(ans);
     }//print
 
-    
-    private static int getDay(int i, int j, int limit) {
+    private static int getDay(int a, int b, int limit) {
         int start = 1;
-        int end = K+1;
-        int mid;
+        int end = K + 1;
+        int mid, totalH;
 
         while(start < end) {
             mid = (start + end) / 2;
+            // a랑 b의 mid날째 키 합
+            totalH = getHeight(a, mid) + getHeight(b, mid);
 
-            int sum = getHeight(i, mid) + getHeight(j, mid);
-
-            if(sum < limit) start = mid + 1;
-            else end = mid;
+            if (totalH >= limit) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
         }
 
         return end;
     }//getDay
 
-    
-    private static int getHeight(int num, int day) {
+    private static int getHeight(int idx, int day) {
+        List<Integer> h = height.get(idx);
         int start = 0;
-        int end = growList.get(num).size() - 1;
+        int end = h.size();
         int mid;
 
-        while(start <= end) {
+        while(start < end) {
             mid = (start + end) / 2;
 
-            if(growList.get(num).get(mid) <= day) start = mid + 1;
-            else end = mid - 1;
+            if (h.get(mid) > day) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
         }
 
-        return end + 1;
+        return start;
     }//getHeight
 
-    
-    private static void init() throws IOException {
+    private static void sol() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 아이들의 수 N, 놀이기구의 수 M, 기간 K, 질문의 개수 Q가 주어진다.
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 아이들의 수
-        M = Integer.parseInt(st.nextToken()); // 놀이기구의 수
+        int N = Integer.parseInt(st.nextToken()); // 아이들의 수
+        int M = Integer.parseInt(st.nextToken()); // 놀이기구의 수
         K = Integer.parseInt(st.nextToken()); // 기간
-        Q = Integer.parseInt(st.nextToken()); // 질문의 개수
+        int Q = Integer.parseInt(st.nextToken()); // 질문의 개수
 
-        limits = new int[M+1]; // 놀이기구들의 키제한
-        days = new int[K+1]; // 각 날마다 놀이기구 타는 횟수
-        growList = new ArrayList<>(N+1);
+        int[] limits = new int[M+1]; // 놀이기구 키제한
+        days = new int[K+1]; // 날마다 아이들이 놀이기구 타는 횟수
+        height = new ArrayList<>(N+1); // 아이들 키 정보
 
         for(int i=0; i<=N; i++) {
-            growList.add(new ArrayList<>());
+            height.add(new ArrayList<>());
         }
 
         // 놀이기구들의 키제한이 순서대로 주어진다.
         st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=M; i++){
+        for(int i=1; i<=M; i++) {
             limits[i] = Integer.parseInt(st.nextToken());
         }
 
         // 각 날에 키가 자라는 아이의 번호가 총 K개 주어진다.
         st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=K; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            growList.get(num).add(i);
+        for(int day=1; day<=K; day++) {
+            int idx = Integer.parseInt(st.nextToken());
+            height.get(idx).add(day);
         }
 
-        while(Q-- > 0) {
+        while(Q-- >0) {
             st = new StringTokenizer(br.readLine());
-            // 아이 i와 아이 j가 놀이기구 k를 타려고 매일 시도
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+            // a랑 b가 놀이기구 m을 타려고 매일 시도함
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-            int day = getDay(i, j, limits[k]);
-
-            if(day <= K) days[day]++;
+            // a랑 b가 놀이기구 m을 탈 수 있는 날
+            int day = getDay(a, b, limits[m]);
+            if (day <= K) {
+                days[day]++;
+            }
         }
 
-    }//init
-
+        br.close();
+    }//sol
 
 }//class
