d1285bf5b187b308233b4a4c41c7786d5179447c
minjeong <79951187+ealswjd@users.noreply.github.com>
2026-02-05 18:53:03 +0900

    [Gold III] Title: 순회강연, Time: 232 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,61 +1,102 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/2109
 public class Main {
-    static final int P = 0, D = 1;
-    static int N;
+    private static final int DAY = 10_000;
+    private static int N; // 대학 개수
+    private static University[] university;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 대학 개수
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if(o1[P] == o2[P]) return o1[D] - o2[D];
-            return o2[P] - o1[P];
-        });
-
-        StringTokenizer st;
-        int maxDay = 0;
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int pay = Integer.parseInt(st.nextToken()); // 강연료
-            int day = Integer.parseInt(st.nextToken()); // 강연일
-
-            pq.offer(new int[] {pay, day});
-            maxDay = Math.max(maxDay, day);
-        }
-
-        int total = getTotal(maxDay, pq);
-        System.out.print(total);
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
     }//main
 
-    
-    private static int getTotal(int maxDay, PriorityQueue<int[]> pq) {
-        int sum = 0;
-        int day, pay;
-        int[] cur;
-        boolean[] checked = new boolean[maxDay+1];
+    private static void sol() {
+        boolean[] checked = new boolean[DAY+1];
+        int total = 0;
+        int pay, day;
 
-        while(!pq.isEmpty()) {
-            cur = pq.poll();
-            pay = cur[P];
-            day = cur[D];
+        for(int i=0; i<N; i++) {
+            pay = university[i].pay;
+            day = university[i].day;
 
-            for(int d=day; d>0; d--) {
-                if(!checked[d]) {
-                    sum += pay;
-                    checked[d] = true;
-                    
-                    break;
-                }
+            while (day > 1 && checked[day]) {
+                day--;
+            }
+
+            if (!checked[day]) {
+                checked[day] = true;
+                total += pay;
             }
         }
 
-        return sum;
-    }//getTotal
+        System.out.print(total);
+    }//sol
 
-    
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 대학 개수
+
+        university = new University[N]; // 각 대학별 강연료, 날짜
+
+        StringTokenizer st;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            university[i] = new University(p, d);
+        }
+
+        Arrays.sort(university);
+        br.close();
+    }//init
+
+    private static class University implements Comparable<University> {
+        int pay;
+        int day;
+
+        University(int pay, int day) {
+            this.pay = pay;
+            this.day = day;
+        }
+
+        @Override
+        public int compareTo(University u) {
+            if (this.pay == u.pay) {
+                return u.day - this.day;
+            }
+            return u.pay - this.pay;
+        }
+    }//University
+
 }//class
+/*
+20
+85 8
+56 11
+58 12
+28 20
+36 12
+45 9
+55 4
+1 3
+71 6
+72 15
+38 9
+76 20
+67 5
+78 2
+48 18
+100 3
+16 2
+7 10
+95 5
+42 14
+---
+1050
+ */
