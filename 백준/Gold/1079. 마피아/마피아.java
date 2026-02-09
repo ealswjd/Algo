81fa471d33183107d94d720af127f854fb6bd7b9
minjeong <79951187+ealswjd@users.noreply.github.com>
2026-02-09 18:19:22 +0900

    [Gold II] Title: 마피아, Time: 184 ms, Memory: 16976 KB -BaekjoonHub

--- 백준/Gold/1079. 마피아/마피아.java
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/1079
 public class Main {
     private static int N, M; // 참가자의 수, 마피아 참가 번호
-    private static int max;
+    private static int maxNight; // 마피아가 버틸 수 있는 최댓값
     private static int[] score; // 유죄 점수
     private static int[][] R; // 참가자 탈락 시 변하는 유죄 점수
 
@@ -19,37 +19,51 @@ public class Main {
         // 짝:밤 홀:낮
         dfs(N, 0, new boolean[N]);
 
-        System.out.print(max);
+        System.out.print(maxNight);
     }//sol
 
     private static void dfs(int cnt, int night, boolean[] isDead) {
-        if (cnt == 1) {
-            max = Math.max(max, night);
+        // 이미 최대치를 찾았거나 마피아가 진 경우
+        if (maxNight == N/2 || isDead[M]) {
+            maxNight = Math.max(maxNight, night);
             return;
         }
-        if (isDead[M]) return;
+        // 참가자가 1명 남음 = 게임 종료
+        if (cnt == 1) {
+            maxNight = Math.max(maxNight, night);
+            return;
+        }
 
         if (cnt % 2 == 0) { // 밤
             // 밤에는 마피아가 죽일 사람을 한 명 고른다. 이 경우 각 사람의 유죄 지수가 바뀐다
             for(int i=0; i<N; i++) {
+                // 이미 죽었거나 마피아 자기 자신이면 패스
                 if (isDead[i] || i == M) continue;
 
+                // i번 사람 죽이고 점수 변경
                 isDead[i] = true;
                 changeScore(i, 1);
                 dfs(cnt-1, night+1, isDead);
 
+                // 복구
                 changeScore(i, -1);
                 isDead[i] = false;
+
+                // 이미 최대치 찾음
+                if (maxNight == N/2) return;
             }
         } else { // 낮
             // 낮에는 현재 게임에 남아있는 사람 중에 유죄 지수가 가장 높은 사람을 죽인다.
             int idx = kill(isDead);
             isDead[idx] = true;
+
             if (idx == M) {
-                max = Math.max(max, night);
+                // 마피아가 졌음
+                maxNight = Math.max(maxNight, night);
                 isDead[idx] = false;
                 return;
             }
+
             dfs(cnt-1, night, isDead);
             isDead[idx] = false;
         }
