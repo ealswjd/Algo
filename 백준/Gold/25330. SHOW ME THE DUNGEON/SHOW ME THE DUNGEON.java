1da5e488e955f69a170bc8a722983c8dafc2938a
minjeong <79951187+ealswjd@users.noreply.github.com>
2026-02-19 21:01:43 +0900

    [Gold IV] Title: SHOW ME THE DUNGEON, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.java
+++ 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.java
@@ -2,16 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/25330
 public class Main {
-    private static final int A = 0, P = 1;
-
     private static int N, K; // 몬스터의 수, 초기 체력
     private static int max; // 해방시킬 수 있는 주민들의 최대 수
-    private static int[][] info; // 각 마을에 있는 몬스터의 공격력, 주민의 수
+    private static Village[] villages; // 각 마을에 있는 몬스터의 공격력, 주민의 수
+
+    private static class Village implements Comparable<Village> {
+        int a, p;
+        Village(int a, int p) {
+            this.a = a;
+            this.p = p;
+        }
+        @Override
+        public int compareTo(Village o) {
+            return this.a - o.a; // 공격력 오름차순 정렬
+        }
+    }//Village
 
     public static void main(String[] args) throws IOException {
         init();
@@ -24,9 +33,9 @@ public class Main {
         max = Math.max(max, people);
 
         for(int i=cur; i<N; i++) {
-            int sum = prev + info[i][A]; // 이전 공격들 합 + 현재 공격
+            int sum = prev + villages[i].a; // 이전 공격들 합 + 현재 공격
             int nDamage = damage + sum; // 지금까지 입은 공격 + 이번 마을에서의 공격
-            int nPeople = people + info[i][P]; // 구할 수 있는 주민의 수
+            int nPeople = people + villages[i].p; // 구할 수 있는 주민의 수
 
             if (nDamage <= K) {
                 sol(i + 1, nDamage, sum, nPeople);
@@ -45,7 +54,7 @@ public class Main {
 
         int[] a = new int[N]; // 공격력
         int[] p = new int[N]; // 주민의 수
-        info = new int[N][2]; // 각 마을의 공격력, 주민의 수
+        villages = new Village[N]; // 각 마을의 공격력, 주민의 수
 
         // 각 마을에 있는 몬스터의 공격력
         st = new StringTokenizer(br.readLine());
@@ -61,11 +70,10 @@ public class Main {
         br.close();
 
         for(int i=0; i<N; i++) {
-            info[i][0] = a[i];
-            info[i][1] = p[i];
+            villages[i] = new Village(a[i], p[i]);
         }
 
-        Arrays.sort(info, Comparator.comparingInt(o -> o[A]));
+        Arrays.sort(villages);
     }//init
 
 }//class
